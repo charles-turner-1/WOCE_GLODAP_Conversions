d1850be1fff16033926026f7deb5d7ea0442050b
charles-turner-1 <ct6g18@soton.ac.uk>
2021-07-04 14:45:15 +0100

    Adding additional detail to WOCE codenames

--- createSectionNames.sh
+++ createSectionNames.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while IFS= read -r line; do    
-    outputName=$(printf '%s.txt' "${line}")    
-    printf "WOCE Section %s: Creating Expocode file.\n" "${line}"    
+	section=$(echo "${line}" | cut -d',' -f1)
+    outputName=$(printf '%s.csv' "${section}")    
+    echo "WOCE Section $section: Creating Expocode file." 
 	touch $outputName
     printf "Adding header info\n"    
 	echo "Year,  GLODAP Expocode,GO-SHIP Expocode" > $outputName
